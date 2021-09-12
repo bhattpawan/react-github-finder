707f35c0102d96523c2f64a690f4d3132f2e3574
bhattpawan <bhattpawan1997@gmail.com>
2021-09-13 00:00:03 +0530

    minor changes

--- src/Components/Users/SearchUser.jsx
+++ src/Components/Users/SearchUser.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import { useState } from 'react';
 
 const SearchUser = ({ searchUsers }) => {
+  const [user, setUser] = useState('');
+
   const getUser = (e) => {
     e.preventDefault();
     if (e.target.value !== '') {
-      searchUsers(e.target.value);
+      setUser(e.target.value);
+    }
+  };
+
+  const searchUser = () => {
+    if (user !== '') {
+      searchUsers(user);
     }
   };
 
   return (
-    <form className='my-4'>
-      <input type='text' className='form-control' id='user' name='user' placeholder='Search User...' onChange={getUser} />
+    <form className='form-row my-3' onSubmit={searchUser}>
+      <div className='form-col'>
+        <input type='text' className='form-control' id='user' name='user' placeholder='Search User...' onChange={getUser} />
+      </div>
+
+      <div className='form-col'>
+        <button type='submit' className='btn btn-outline-dark'>
+          Search
+        </button>
+      </div>
     </form>
   );
 };
