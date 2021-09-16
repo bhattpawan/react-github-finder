3551b169053892f3e129aa1e4acf7186342010e5
bhattpawan <bhattpawan1997@gmail.com>
2021-09-17 00:03:50 +0530

    Added single user component

--- src/Components/Users/UserDetail.jsx
+++ src/Components/Users/UserDetail.jsx
@@ -8,15 +8,50 @@ const UserDetail = ({ user, showCurrentUser }) => {
     showCurrentUser(username);
   }, []);
 
-  const { name, avatar_url, location, bio, blog, login, html_url, followers, following, public_repos, public_gists, hireable } = user;
+  const { name, avatar_url, location, bio, blog, company, html_url, followers, following, public_repos, public_gists } = user;
 
   return (
     <>
       <Link to='/'>Back To Search</Link>
       <section className='current-user'>
-        Hireable: {hireable ? <span className='hireable success'>&#10003;</span> : <span className='hireable danger'>&times;</span>}
         <div className='user-details'>
           <img src={avatar_url} alt='' />
+          <h1>{name}</h1>
+          <p>{location}</p>
+          <p>
+            {bio && (
+              <span>
+                <strong>Bio:</strong> {bio}
+              </span>
+            )}
+          </p>
+          <a href={html_url} target='_blank'>
+            View GitHub
+          </a>
+          <ul>
+            <li>
+              {blog && (
+                <span>
+                  <strong>Blog: </strong>
+                  {blog}
+                </span>
+              )}
+            </li>
+            <li>
+              {company && (
+                <span>
+                  <strong>Company: </strong>
+                  {company}
+                </span>
+              )}
+            </li>
+          </ul>
+          <div className='followers-container'>
+            <div className='badge'>Followers: {followers}</div>
+            <div className='badge'>Following: {following}</div>
+            <div className='badge'>Public Repos: {public_repos}</div>
+            <div className='badge'>Public Gists: {public_gists}</div>
+          </div>
         </div>
       </section>
     </>
