a4e568aa5a9b02cfbb64f1d83de4f0d01ed2a5fd
bhattpawan <bhattpawan1997@gmail.com>
2021-10-04 10:28:51 +0530

    404 page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Components/Layout/Navbar';
 import Home from './Components/Pages/Home';
 import About from './Components/Pages/About';
+import NotFound from './Components/Pages/NotFound';
 import Container from './Components/Layout/Container';
 import UserDetail from './Components/Users/UserDetail';
 import { GithubContextProvider } from './Context/GithubContext';
@@ -23,6 +24,9 @@ const App = () => {
           </GithubContextProvider>
           <Route path='/about' exact>
             <About />
+          </Route>
+          <Route path='/*'>
+            <NotFound />
           </Route>
         </Container>
       </Switch>
